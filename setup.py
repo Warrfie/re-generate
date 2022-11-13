db704d51933501f4d17bae7b3a6985f0340631d3
Warrfie <warrfie@gmail.com>
2022-11-13 18:20:15 +0300

    0.1.2 version "Instant bug fix"

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="re-generate",
-    version="0.1.1",
+    version="0.1.2",
     description="Package for random data generation via ReGex",
     long_description=long_description,
     url="https://github.com/Warrfie/re-generate",
